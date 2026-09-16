e700f325c132ab07112a5ab572ca827ea9fb830b
Dewnitha Rivisahan <dewnitharivisahan17794@gmail.com>
2026-09-16 11:42:58 +0530

    1st interface is completed

---
+++ Place_holder.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+
+def PLH(parent, text, var):
+    style=ttk.Style()
+    style.configure("Placeholder.TEntry", forground="Gray")
+    style.configure("Nomal.TEntry", forground="Black")
+
+    style.map(
+        "Placeholder.TEntry",
+        foreground=[("!disabled", "gray")])
+
+    style.map(
+        "Normal.TEntry",
+        foreground=[("!disabled", "black")])
+
+    entry=ttk.Entry(parent, textvariable=var, style="Placeholder.TEntry", width=150)
+    entry.pack(padx=10, pady=10)
+    entry.insert(0,text)
+
+    def forcus_in(event):
+        if entry.get()==text:
+            entry.delete(0,tk.END)
+            entry.configure(style="Nomal.TEntry")
+
+    def forcus_out(event):
+        if entry.get()=="":
+            entry.insert(0,text)
+            entry.configure(style="Placeholder.TEntry")
+            
+    entry.bind("<FocusIn>", forcus_in)
+    entry.bind("<FocusOut>", forcus_out)
--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import tkinter as tk
 from tkinter import ttk
+from Place_holder import PLH
 
 root = tk.Tk()
 title = root.title("SMng")
 icon = root.iconbitmap(r"C:\Users\USER\OneDrive\Documents\GitHub\student grade management system\icon.ico")
-geometry = root.geometry(f'700x700')
+geometry = root.geometry(f'500x500')
+search_Name = tk.StringVar()
+search_ID = tk.StringVar()
 lable = ttk.Label(root, text = "STUDENT MANAGER", foreground="Red", font="arial 30 bold ")
 lable.pack()
+
+frame1 = ttk.Frame(root, width=400, height=150, relief='groove') 
+frame1.pack(pady=20)
+frame1.pack_propagate(False)
+
+PLH(frame1, "Student_ID", search_ID)
+PLH(frame1, 'Student Name', search_Name)
+
+button1 = ttk.Button(frame1, text='All Students')
+button1.pack(side='left', pady=10, padx=10)
+
+button2 = ttk.Button(frame1, text='Add New Student' )
+button2.pack(side='right', pady=10, padx=10)
+
+button3 = ttk.Button(frame1, text='Search' )
+button3.pack(side='bottom', pady=20, padx=10)
+
 root.mainloop()
